5fd2675906e973274360d68b6135f7c9eb11739d
SAVARA KALYAN <savarakalyangvp@gmail.com>
2023-12-19 04:32:46 +0530

    V-1
    This is the demo version of showing posts in web page.

--- src/main/java/org/app/TechnicalBlog/controller/HomeController.java
+++ src/main/java/org/app/TechnicalBlog/controller/HomeController.java
@@ -1,8 +1,11 @@
 package org.app.TechnicalBlog.controller;
 
 import org.app.TechnicalBlog.model.Post;
+import org.app.TechnicalBlog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -10,33 +13,15 @@ import java.util.Date;
 
 @Controller
 public class HomeController {
-    @ResponseBody
+    @Autowired
+    private PostService postService;
+    @RequestMapping("/")
     public String getAllPosts(Model model) {
 
-
-        ArrayList<Post> posts = new ArrayList<>();
-
-        Post post1 = new Post();
-        post1.setTitle("Post 1");
-        post1.setBody("Post Body 1");
-        post1.setDate(new Date());
-
-        Post post2 = new Post();
-        post2.setTitle("Post 2");
-        post2.setBody("Post Body 2");
-        post2.setDate(new Date());
-
-        Post post3 = new Post();
-        post3.setTitle("Post 3");
-        post3.setBody("Post Body 3");
-        post3.setDate(new Date());
-
-        posts.add(post1);
-        posts.add(post2);
-        posts.add(post3);
-
+        ArrayList<Post> posts = postService.getAllPosts();
 
         model.addAttribute("posts" , posts);
+
         return "index";
 
     }
