8a3d4a97973d59f8f5ff491f89eb10b9defbc356
SAVARA KALYAN <savarakalyangvp@gmail.com>
2023-12-19 03:36:19 +0530

    Constuct the body for spring
    Using spring initializer, created a body for spring boot web application.

---
+++ src/test/java/org/app/TechnicalBlog/TechnicalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app.TechnicalBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
